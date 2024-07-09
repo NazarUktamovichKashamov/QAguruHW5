14eccd317a8f680623ce1165260b92aa1228d302
kashamovnu <nazar.kashamov@gmail.com>
2024-07-09 20:01:18 +0400

    Запушил правки по комментарию

--- tests_homework/test_hw.py
+++ tests_homework/test_hw.py
@@ -7,7 +7,7 @@ def test_dark_theme_by_time():
     """
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
-    if current_time.hour < 6 or current_time.hour > 22:
+    if current_time.hour < 6 or current_time.hour >= 22:
         is_dark_theme = True
     else:
         is_dark_theme = False
@@ -28,12 +28,12 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    if dark_theme_enabled_by_user == True:
+    if dark_theme_enabled_by_user is True:
         is_dark_theme = True
-    elif dark_theme_enabled_by_user == False:
+    elif dark_theme_enabled_by_user is False:
         is_dark_theme = False
     else:
-        if current_time.hour > 22 or current_time.hour < 6:
+        if current_time.hour >= 22 or current_time.hour < 6:
             is_dark_theme = True
         else:
             is_dark_theme = False
